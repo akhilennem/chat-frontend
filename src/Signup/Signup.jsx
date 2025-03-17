17894031f057ee1828656afc67752c6af7cd4758
akhilnm <akhilnm.techintl@gmail.com>
2025-03-17 15:18:57 +0530

    fixed chat home style

--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -9,8 +9,8 @@ export default function SignupPage() {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
-  const url='https://m4vx17k1-5000.inc1.devtunnels.ms/'
-  // const url='http://localhost:5000/'
+  // const url='https://m4vx17k1-5000.inc1.devtunnels.ms/'
+  const url='http://localhost:5000/'
   const handleSubmit = async (e) => {
     e.preventDefault();
 
